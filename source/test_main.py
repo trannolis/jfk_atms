97e9a154b2f188800af4f5a35c78a9ba2fa44381
FaizanMHussain <72048709+FaizanMHussain@users.noreply.github.com>
2021-05-01 18:41:11 -0400

    Adding testing file

---
+++ source/test_main.py
@@ -0,0 +1,9 @@
+import unittest
+import app
+
+class TestEndpoints(unittest.TestCase):
+  def setUp(self):
+        self.app = app.app.test_client()
+   
+if __name__ == "__main__":
+    unittest.main()
