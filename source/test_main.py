1a93854e163b6e778116ac32f1949e336d66ad5b
FaizanMHussain <72048709+FaizanMHussain@users.noreply.github.com>
2021-05-09 17:40:29 -0400

    Added test endpoints for register Admin and Pilot

--- source/test_main.py
+++ source/test_main.py
@@ -7,11 +7,47 @@ class TestEndpoints(unittest.TestCase):
         self.app = create_app('source.settings', True)
         self.client = self.app.test_client()
 
+    def test_register_admin(self):
+        """
+        Tests registering Air Traffic Controllers
+        """
+        page_html = str(self.client.get('/registerAdmin').data)
+        username_elem = 'Username'
+        password_elem = 'Password'
+        first_name_elem = 'First Name'
+        last_name_elem = 'Last Name'
+        email_elem = 'Email'
+        phone_number_elem = 'Phone No.'
+        self.assertIn(username_elem, page_html)
+        self.assertIn(password_elem, page_html)
+        self.assertIn(first_name_elem, page_html)
+        self.assertIn(last_name_elem, page_html)
+        self.assertIn(email_elem, page_html)
+        self.assertIn(phone_number_elem, page_html)
+
     def test_register_atc(self):
         """
         Tests registering Air Traffic Controllers
         """
         page_html = str(self.client.get('/registerAtc').data)
+        username_elem = 'Username'
+        password_elem = 'Password'
+        first_name_elem = 'First Name'
+        last_name_elem = 'Last Name'
+        email_elem = 'Email'
+        phone_number_elem = 'Phone No.'
+        self.assertIn(username_elem, page_html)
+        self.assertIn(password_elem, page_html)
+        self.assertIn(first_name_elem, page_html)
+        self.assertIn(last_name_elem, page_html)
+        self.assertIn(email_elem, page_html)
+        self.assertIn(phone_number_elem, page_html)
+
+   def test_register_pilot(self):
+        """
+        Tests registering Air Traffic Controllers
+        """
+        page_html = str(self.client.get('/registerPilot').data)
         username_elem = 'Username'
         password_elem = 'Password'
         first_name_elem = 'First Name'
