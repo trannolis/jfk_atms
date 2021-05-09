47201225c899bde4bd5a5a51229b6682940bd5b0
FaizanMHussain <72048709+FaizanMHussain@users.noreply.github.com>
2021-05-09 17:52:55 -0400

    Update test_main.py

--- source/test_main.py
+++ source/test_main.py
@@ -7,7 +7,7 @@ class TestEndpoints(unittest.TestCase):
         self.app = create_app('source.settings', True)
         self.client = self.app.test_client()
 
-    def test_register_atc(self):
+    def test_register_admin(self):
         """
         Tests registering Admins
         """
