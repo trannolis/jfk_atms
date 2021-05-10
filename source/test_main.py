977dbb23a80e1a7e840895b3c8f215e64438c6d2
FaizanMHussain <72048709+FaizanMHussain@users.noreply.github.com>
2021-05-10 01:26:10 -0400

    add endpoint for adminHome

--- source/test_main.py
+++ source/test_main.py
@@ -65,6 +65,20 @@ class TestEndpoints(unittest.TestCase):
 #         self.assertIn(user_elem, page_html)
 #         self.assertIn(logout_elem, page_html)
 
+    def test_admin_home(self):
+        """
+        Tests endpoint for admin home page
+        """
+        page_html = str(self.client.get('adminHome').data)
+        reg_atc_elem = 'Register a new Air Traffic Controller'
+        reg_pilot_elem = 'Register a new Pilot'
+        user_elem = 'Delete an existing user'
+        logout_elem = 'Logout'
+        self.assertIn(reg_atc_elem, page_html)
+        self.assertIn(reg_pilot_elem, page_html)
+        self.assertIn(user_elem, page_html)
+        self.assertIn(logout_elem, page_html)
+
     def test_register_admin(self):
         """
         Tests registering Admins
