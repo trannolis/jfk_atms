7b4445ff5958fe80feb7f1b4de149594b8ced24c
FaizanMHussain <72048709+FaizanMHussain@users.noreply.github.com>
2021-05-15 11:00:28 -0400

    reorder test endpoints to match order in main.py

--- source/test_main.py
+++ source/test_main.py
@@ -25,6 +25,24 @@ class TestEndpoints(unittest.TestCase):
         self.assertIn(username_elem, page_html)
         self.assertIn(password_elem, page_html)
 
+    def test_register_admin(self):
+        """
+        Tests registering Admins
+        """
+        page_html = str(self.client.get('/registerAdmin').data)
+        username_elem = 'Username'
+        password_elem = 'Password'
+        first_name_elem = 'First Name'
+        last_name_elem = 'Last Name'
+        email_elem = 'Email'
+        phone_number_elem = 'Phone No.'
+        self.assertIn(username_elem, page_html)
+        self.assertIn(password_elem, page_html)
+        self.assertIn(first_name_elem, page_html)
+        self.assertIn(last_name_elem, page_html)
+        self.assertIn(email_elem, page_html)
+        self.assertIn(phone_number_elem, page_html)
+
     def test_login_admin(self):
         """
         Tests the ability of an admin to login
@@ -70,6 +88,42 @@ class TestEndpoints(unittest.TestCase):
         self.client.post('/register',
                          data=dict(role="pilot"),
                          follow_redirects=True)
+
+    def test_register_atc(self):
+        """
+        Tests registering Air Traffic Controllers
+        """
+        page_html = str(self.client.get('/registerAtc').data)
+        username_elem = 'Username'
+        password_elem = 'Password'
+        first_name_elem = 'First Name'
+        last_name_elem = 'Last Name'
+        email_elem = 'Email'
+        phone_number_elem = 'Phone No.'
+        self.assertIn(username_elem, page_html)
+        self.assertIn(password_elem, page_html)
+        self.assertIn(first_name_elem, page_html)
+        self.assertIn(last_name_elem, page_html)
+        self.assertIn(email_elem, page_html)
+        self.assertIn(phone_number_elem, page_html)
+
+    def test_register_pilot(self):
+        """
+        Tests registering Pilots
+        """
+        page_html = str(self.client.get('/registerPilot').data)
+        username_elem = 'Username'
+        password_elem = 'Password'
+        first_name_elem = 'First Name'
+        last_name_elem = 'Last Name'
+        email_elem = 'Email'
+        phone_number_elem = 'Phone No.'
+        self.assertIn(username_elem, page_html)
+        self.assertIn(password_elem, page_html)
+        self.assertIn(first_name_elem, page_html)
+        self.assertIn(last_name_elem, page_html)
+        self.assertIn(email_elem, page_html)
+        self.assertIn(phone_number_elem, page_html)
 
     def test_register_auth_admin(self):
         """
@@ -371,60 +425,6 @@ class TestEndpoints(unittest.TestCase):
         self.assertIn(submit_elem, page_html)
         self.assertIn(back_elem, page_html)
         self.assertIn(logout_elem, page_html)
-
-    def test_register_admin(self):
-        """
-        Tests registering Admins
-        """
-        page_html = str(self.client.get('/registerAdmin').data)
-        username_elem = 'Username'
-        password_elem = 'Password'
-        first_name_elem = 'First Name'
-        last_name_elem = 'Last Name'
-        email_elem = 'Email'
-        phone_number_elem = 'Phone No.'
-        self.assertIn(username_elem, page_html)
-        self.assertIn(password_elem, page_html)
-        self.assertIn(first_name_elem, page_html)
-        self.assertIn(last_name_elem, page_html)
-        self.assertIn(email_elem, page_html)
-        self.assertIn(phone_number_elem, page_html)
-
-    def test_register_atc(self):
-        """
-        Tests registering Air Traffic Controllers
-        """
-        page_html = str(self.client.get('/registerAtc').data)
-        username_elem = 'Username'
-        password_elem = 'Password'
-        first_name_elem = 'First Name'
-        last_name_elem = 'Last Name'
-        email_elem = 'Email'
-        phone_number_elem = 'Phone No.'
-        self.assertIn(username_elem, page_html)
-        self.assertIn(password_elem, page_html)
-        self.assertIn(first_name_elem, page_html)
-        self.assertIn(last_name_elem, page_html)
-        self.assertIn(email_elem, page_html)
-        self.assertIn(phone_number_elem, page_html)
-
-    def test_register_pilot(self):
-        """
-        Tests registering Pilots
-        """
-        page_html = str(self.client.get('/registerPilot').data)
-        username_elem = 'Username'
-        password_elem = 'Password'
-        first_name_elem = 'First Name'
-        last_name_elem = 'Last Name'
-        email_elem = 'Email'
-        phone_number_elem = 'Phone No.'
-        self.assertIn(username_elem, page_html)
-        self.assertIn(password_elem, page_html)
-        self.assertIn(first_name_elem, page_html)
-        self.assertIn(last_name_elem, page_html)
-        self.assertIn(email_elem, page_html)
-        self.assertIn(phone_number_elem, page_html)
 
 
 if __name__ == "__main__":
