c5cad603d384a224c7762bb0c349c11f648712cc
FaizanMHussain <72048709+FaizanMHussain@users.noreply.github.com>
2021-05-15 11:46:29 -0400

    Add endpoint for changing gates

--- source/test_main.py
+++ source/test_main.py
@@ -520,6 +520,49 @@ class TestEndpoints(unittest.TestCase):
         self.assertIn(back_elem, page_html)
         self.assertIn(logout_elem, page_html)
 
+    def test_change_gate(self):
+        """
+        Tests changing gates
+        """
+        self.client.post('/changeGate',
+                         data=dict(select="6"),
+                         follow_redirects=True)
+        page_html = str(self.client.get('/getFlights').data)
+        title_elem = 'All Scheduled Flights'
+        select_elem = 'Select'
+        id_elem = 'ID'
+        atime_elem = 'Arrival Time'
+        dtime_elem = 'Departure Time'
+        dloc_elem = 'Departure Location'
+        aloc_elem = 'Arrival Location'
+        plane_elem = 'Airplane ID'
+        gate_elem = 'Gate Number'
+        runway_elem = 'Runway Number'
+        updateg_elem = 'Update Gate'
+        updater_elem = 'Update Runway'
+        freeg_elem = 'Free Up Gate'
+        freer_elem = 'Free Up Runway'
+        submit_elem = 'Continue'
+        back_elem = 'Back'
+        logout_elem = 'Logout'
+        self.assertIn(title_elem, page_html)
+        self.assertIn(select_elem, page_html)
+        self.assertIn(id_elem, page_html)
+        self.assertIn(atime_elem, page_html)
+        self.assertIn(dtime_elem, page_html)
+        self.assertIn(dloc_elem, page_html)
+        self.assertIn(aloc_elem, page_html)
+        self.assertIn(plane_elem, page_html)
+        self.assertIn(gate_elem, page_html)
+        self.assertIn(runway_elem, page_html)
+        self.assertIn(updateg_elem, page_html)
+        self.assertIn(updater_elem, page_html)
+        self.assertIn(freeg_elem, page_html)
+        self.assertIn(freer_elem, page_html)
+        self.assertIn(submit_elem, page_html)
+        self.assertIn(back_elem, page_html)
+        self.assertIn(logout_elem, page_html)        
+
 
 if __name__ == "__main__":
     unittest.main()
