466daa44bca7beca3995dca9fed4ba4f0e2e05d3
FaizanMHussain <72048709+FaizanMHussain@users.noreply.github.com>
2021-05-14 23:31:59 -0400

    update admin home endpoint

--- source/test_main.py
+++ source/test_main.py
@@ -126,15 +126,15 @@ class TestEndpoints(unittest.TestCase):
         self.client.post('/adminHome',
                          data=dict(username="admin"),
                          follow_redirects=False)
-#         page_html = str(self.client.get('adminHome').data)
-#         reg_atc_elem = 'Register a new Air Traffic Controller'
-#         reg_pilot_elem = 'Register a new Pilot'
-#         user_elem = 'Delete an existing user'
-#         logout_elem = 'Logout'
-#         self.assertIn(reg_atc_elem, page_html)
-#         self.assertIn(reg_pilot_elem, page_html)
-#         self.assertIn(user_elem, page_html)
-#         self.assertIn(logout_elem, page_html)
+        page_html = str(self.client.get('adminHome').data)
+        reg_atc_elem = 'Register a new Air Traffic Controller'
+        reg_pilot_elem = 'Register a new Pilot'
+        user_elem = 'Delete an existing user'
+        logout_elem = 'Logout'
+        self.assertIn(reg_atc_elem, page_html)
+        self.assertIn(reg_pilot_elem, page_html)
+        self.assertIn(user_elem, page_html)
+        self.assertIn(logout_elem, page_html)
 
     def test_show_user(self):
         """
