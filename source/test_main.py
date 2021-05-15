5b53caac5fd597c55080e01d9ad499076f296785
FaizanMHussain <72048709+FaizanMHussain@users.noreply.github.com>
2021-05-15 17:16:38 -0400

    Update showFlights endpoint

--- source/test_main.py
+++ source/test_main.py
@@ -394,8 +394,30 @@ class TestEndpoints(unittest.TestCase):
                                    update="gate",
                                    vacate="freeGate"),
                          follow_redirects=True)
-        page_html = str(self.client.get('/showFlights').data)
-        print(page_html)
+        page_html = str(self.client.get('/vacantGates').data)
+        title_elem = 'Available Gates'
+        select_elem = 'Select'
+        id_elem = 'ID'
+        x_elem = 'X_coord'
+        y_elem = 'Y_coord'
+        vacant_elem = 'Is vacant'
+        service_elem = 'In service'
+        terminal_elem = 'Terminal'
+        submit_elem = 'Continue'
+        back_elem = 'Back'
+        logout_elem = 'Logout'
+        self.assertIn(title_elem, page_html)
+        self.assertIn(select_elem, page_html)
+        self.assertIn(id_elem, page_html)
+        self.assertIn(x_elem, page_html)
+        self.assertIn(y_elem, page_html)
+        self.assertIn(vacant_elem, page_html)
+        self.assertIn(service_elem, page_html)
+        self.assertIn(terminal_elem, page_html)
+        self.assertIn(submit_elem, page_html)
+        self.assertIn(back_elem, page_html)
+        self.assertIn(logout_elem, page_html)
+
 
     def test_occupied_gates(self):
         """
