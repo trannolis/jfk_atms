d9d10e13ad3e4a7db6b1108147099d640a0af0ec
FaizanMHussain <72048709+FaizanMHussain@users.noreply.github.com>
2021-05-15 15:32:23 -0400

    Update '/registerAdmin' endpoint

--- source/test_main.py
+++ source/test_main.py
@@ -44,18 +44,24 @@ class TestEndpoints(unittest.TestCase):
         Tests registering Admins
         """
         page_html = str(self.client.get('/registerAdmin').data)
+        title_elem = 'Register A New Admin'
         username_elem = 'Username'
         password_elem = 'Password'
         first_name_elem = 'First Name'
         last_name_elem = 'Last Name'
         email_elem = 'Email'
         phone_number_elem = 'Phone No.'
+        submit_elem = 'Register'
+        back_elem = 'Go back'
+        self.assertIn(title_elem, page_html)
         self.assertIn(username_elem, page_html)
         self.assertIn(password_elem, page_html)
         self.assertIn(first_name_elem, page_html)
         self.assertIn(last_name_elem, page_html)
         self.assertIn(email_elem, page_html)
         self.assertIn(phone_number_elem, page_html)
+        self.assertIn(submit_elem, page_html)
+        self.assertIn(back_elem, page_html)
 
     def test_login_admin(self):
         """
