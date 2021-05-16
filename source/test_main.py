1bf81b89c12215f80b4865af8d577e1ba358d25e
FaizanMHussain <72048709+FaizanMHussain@users.noreply.github.com>
2021-05-15 21:48:52 -0400

    Update test_main.py

--- source/test_main.py
+++ source/test_main.py
@@ -106,10 +106,9 @@ class TestEndpoints(unittest.TestCase):
                                     follow_redirects=False)
         self.assertEqual(response.status_code, 302)
         print(response.location)
-
-#         page_html = str(self.client.get('/registerAtc').data)
-#         print("Test 1")
-#         print(page_html)
+        page_html = str(self.client.get('/registerAtc').data)
+        print("Test 1")
+        print(page_html)
 
     def test_register_redirect_pilot(self):
         """
